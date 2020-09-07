74199f454310672fca4b2bff7f54655a675df2dd
Sachin Joseph <6994498+sachinjoseph@users.noreply.github.com>
2020-09-07 02:04:32 -0700

    Trim package size

--- mypkg/repro.ps1
+++ mypkg/repro.ps1
@@ -1,11 +1,11 @@
 echo "Setting up repro"
-rimraf .\node_modules .\pnpm-lock.yaml 
+rimraf .\node_modules .\pnpm-lock.yaml # install without lockfile present
 pnpm install --store D:\.pnpm-store --no-prefer-frozen-lockfile --strict-peer-dependencies
-ls .\node_modules\typescript # directory exists
+ls .\node_modules\typescript # directory exists, yay!
 
 Write-Host
 
 echo "Running repro"
-rimraf .\node_modules
+rimraf .\node_modules # install with lockfile generated above
 pnpm install --store D:\.pnpm-store --no-prefer-frozen-lockfile --strict-peer-dependencies
-ls .\node_modules\typescript # directory doesn't exist
+ls .\node_modules\typescript # directory doesn't exist :(
